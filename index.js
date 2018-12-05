228b2cae433b88ab32ad420b97371d245fc958c5
osamaasaid <osamaasaid@gmail.com>
2018-12-05 01:40:48 -0800

    setup routes and views

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ const dotenv = require('dotenv/config');
 
 const app = express();
 
-app.set('view engine', 'ejs'):
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//views
+app.get('/', (req,res) => {
+    res.render('pages/home')
+});
+
+app.get('programs', (req,res) => {
+    res.render('pages/programs')
+});
+
+app.get('contact', (req,res) => {
+    res.render('pages/contact')
+});
+
+
+//server
+app.listen(process.env.PORT || 3000,
+() => console.log("hello there!"));
